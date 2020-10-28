81e67beab5664f5910652573b1248b61664cccbe
Francesco <francesco.savignano96@gmail.com>
2020-10-28 17:59:20 +0100

    impostata comparsa e sacomparsa dei dropdown-mwnu con mouseenter() e mouseleave()

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-    
+
+    $('.header-right .item').mouseenter(function(){
+
+        $('.dropdown-menu').removeClass('active');
+        $(this).next('.dropdown-menu').addClass('active');
+
+    });
+
+    $('.dropdown-menu').mouseleave(function(){
+        $(this).removeClass('active');
+    });
+
 });
